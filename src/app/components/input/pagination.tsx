74f595e11a37ae4f003dacef7bac3846d131bb4c
Liam <liam@fascinated.cc>
2024-09-11 21:27:32 +0100

    es lint !

--- src/app/components/input/pagination.tsx
+++ src/app/components/input/pagination.tsx
@@ -52,7 +52,7 @@ export default function Pagination({ mobilePagination, page, totalPages, onPageC
     const pageNumbers = [];
     const maxPagesToShow = mobilePagination ? 3 : 4;
     let startPage = Math.max(1, currentPage - Math.floor(maxPagesToShow / 2));
-    let endPage = Math.min(totalPages, startPage + maxPagesToShow - 1);
+    const endPage = Math.min(totalPages, startPage + maxPagesToShow - 1);
 
     if (endPage - startPage < maxPagesToShow - 1) {
       startPage = Math.max(1, endPage - maxPagesToShow + 1);
