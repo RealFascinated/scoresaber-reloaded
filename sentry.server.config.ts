fb7928259ac7b42f860793bc4ab81231ec729dcb
Liam <liam@fascinated.cc>
2024-09-30 09:44:28 +0100

    disable Sentry in dev

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -6,10 +6,7 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://69aed8b4a32e45db8fcb1b4285b4370f@glitchtip.fascinated.cc/13",
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+  enabled: process.env.NODE_ENV === "production",
 });
