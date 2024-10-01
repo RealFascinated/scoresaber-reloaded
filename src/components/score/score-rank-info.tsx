008d3298a6c38675e01f0338a77ae3ec40e00267
Liam <liam@fascinated.cc>
2024-10-01 09:53:39 +0100

    fix cursor when hovering on score rank

--- src/components/score/score-rank-info.tsx
+++ src/components/score/score-rank-info.tsx
@@ -14,7 +14,7 @@ export default function ScoreRankInfo({ score }: Props) {
     <div className="flex w-full flex-row justify-between lg:w-[125px] lg:flex-col lg:justify-center items-center">
       <div className="flex gap-1 items-center">
         <GlobeAmericasIcon className="w-5 h-5" />
-        <p className="text-pp">#{formatNumberWithCommas(score.rank)}</p>
+        <p className="text-pp cursor-default">#{formatNumberWithCommas(score.rank)}</p>
       </div>
       <Tooltip
         display={
