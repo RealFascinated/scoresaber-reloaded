9f13c2f66ac2807159c0f83d0886a77e77d9e708
Liam <liam@fascinated.cc>
2024-10-02 10:59:00 +0100

    no one cares.

--- src/components/chart/generic-chart.tsx
+++ src/components/chart/generic-chart.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import { CategoryScale, Chart, Legend, LinearScale, LineElement, PointElement, Title, Tooltip } from "chart.js";
