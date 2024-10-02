ec694e97fa0b9e1e6b149708c40a81a1871450f4
Liam <liam@fascinated.cc>
2024-10-02 14:08:57 +0100

    now?

---
+++ Backend/src/main/java/cc/fascinated/ssr/Main.java
@@ -0,0 +1,10 @@
+package cc.fascinated.ssr;
+
+/**
+ * @author Fascinated (fascinated7)
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
