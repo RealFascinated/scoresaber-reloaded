5ba781a3a93e14d677a7e39f711850fd70786ec5
Liam <liam@fascinated.cc>
2024-10-03 20:04:03 +0100

    idc rn will fix later

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,11 +1,5 @@
 import { withSentryConfig } from "@sentry/nextjs";
 import { format } from "@formkit/tempo";
-import nextBuildId from "next-build-id";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -25,7 +19,7 @@ const nextConfig = {
   },
   env: {
     NEXT_PUBLIC_BUILD_ID:
-      process.env.GIT_REV || nextBuildId.sync({ dir: __dirname }),
+      process.env.GIT_REV || "dev",
     NEXT_PUBLIC_BUILD_TIME: new Date().toLocaleDateString("en-US", {
       year: "numeric",
       month: "long",
