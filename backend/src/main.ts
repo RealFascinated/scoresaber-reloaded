296179a10b91c7d61d70edf1a7cf8673aa17dc89
Liam <liam@fascinated.cc>
2024-10-04 23:26:05 +0100

    switch backend to fastify

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from "@nestjs/core";
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ["error", "warn", "log", "fatal"],
-  });
-  await app.listen(8080);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: {
+        transport: {
+          target: "@fastify/one-line-logger",
+        },
+      },
+    }),
+    {
+      logger: ["error", "warn", "log"],
+    }
+  );
+  await app.listen(8080, "0.0.0.0");
 }
 bootstrap();
