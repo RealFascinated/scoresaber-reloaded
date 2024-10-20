a68e53734d63b680bc1427cd7658825dcf3ddb9c
Liam <liam@fascinated.cc>
2024-10-20 14:04:55 +0100

    add default stale time to queries

--- projects/website/src/components/providers/query-provider.tsx
+++ projects/website/src/components/providers/query-provider.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
 
 type Props = {
-  children: React.ReactNode;
+  children: ReactNode;
 };
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 30, // 30 seconds
+    },
+  },
+});
 
 export function QueryProvider({ children }: Props) {
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
