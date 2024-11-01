4a9da71a91e356fe78e8ac439dd46c0f06692b86
Liam <liam@fascinated.cc>
2024-11-01 22:53:59 +0000

    fix github logo colors

--- projects/website/src/components/logos/logos/github-logo.tsx
+++ projects/website/src/components/logos/logos/github-logo.tsx
@@ -1,6 +1,9 @@
 import LogoBase from "@/components/logos/logo-base";
 import { LogoProps } from "@/components/logos/logo-props";
+import { cn } from "@/common/utils";
 
 export default function GithubLogo({ size = 32, className }: LogoProps) {
-  return <LogoBase size={size} href={"/assets/logos/github.png"} alt={"Github Logo"} className={className} />;
+  return (
+    <LogoBase size={size} href={"/assets/logos/github.png"} alt={"Github Logo"} className={cn("invert", className)} />
+  );
 }
