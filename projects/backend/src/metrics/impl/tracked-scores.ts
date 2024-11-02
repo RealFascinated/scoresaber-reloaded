9da2300a8f2dc54626a28a4c1d004112618bd2e7
Liam <liam@fascinated.cc>
2024-11-02 03:48:40 +0000

    my poor db :(

--- projects/backend/src/metrics/impl/tracked-scores.ts
+++ projects/backend/src/metrics/impl/tracked-scores.ts
@@ -8,6 +8,6 @@ export default class TrackedScoresMetric extends Metric {
   }
 
   async collect(): Promise<Point> {
-    return this.getPointBase().intField("count", await ScoreSaberScoreModel.countDocuments({}));
+    return this.getPointBase().intField("count", await ScoreSaberScoreModel.estimatedDocumentCount({}));
   }
 }
