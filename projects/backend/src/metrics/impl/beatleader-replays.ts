eb5edfadc8f01a7cb72fa19cdf77648f3dd74f7d
Liam <liam@fascinated.cc>
2024-11-02 03:37:36 +0000

    more metrics!!

--- projects/backend/src/metrics/impl/beatleader-replays.ts
+++ projects/backend/src/metrics/impl/beatleader-replays.ts
@@ -2,6 +2,7 @@ import Metric from "../metric";
 import { Point } from "@influxdata/influxdb-client";
 import MinioService from "../../service/minio.service";
 import { MinioBucket } from "@ssr/common/minio-buckets";
+import { AdditionalScoreDataModel } from "@ssr/common/model/additional-score-data/additional-score-data";
 
 export default class BeatLeaderReplaysMetric extends Metric {
   constructor() {
@@ -9,6 +10,8 @@ export default class BeatLeaderReplaysMetric extends Metric {
   }
 
   async collect(): Promise<Point> {
-    return this.getPointBase().intField("size", await MinioService.getBucketSize(MinioBucket.BeatLeaderReplays));
+    return this.getPointBase()
+      .intField("count", await AdditionalScoreDataModel.countDocuments({ cachedReplayId: { $exists: true } }))
+      .intField("size", await MinioService.getBucketSize(MinioBucket.BeatLeaderReplays));
   }
 }
--- projects/backend/src/metrics/impl/beatleader-score-stats.ts
+++ projects/backend/src/metrics/impl/beatleader-score-stats.ts
@@ -1,6 +1,8 @@
 import Metric from "../metric";
 import { Point } from "@influxdata/influxdb-client";
 import { AdditionalScoreDataModel } from "@ssr/common/model/additional-score-data/additional-score-data";
+import MinioService from "../../service/minio.service";
+import { MinioBucket } from "@ssr/common/minio-buckets";
 
 export default class BeatLeaderScoreStatsMetric extends Metric {
   constructor() {
@@ -8,9 +10,8 @@ export default class BeatLeaderScoreStatsMetric extends Metric {
   }
 
   async collect(): Promise<Point> {
-    return this.getPointBase().intField(
-      "count",
-      await AdditionalScoreDataModel.countDocuments({ cachedScoreStats: true })
-    );
+    return this.getPointBase()
+      .intField("count", await AdditionalScoreDataModel.countDocuments({ cachedScoreStats: true }))
+      .intField("size", await MinioService.getBucketSize(MinioBucket.BeatLeaderScoreStats));
   }
 }
