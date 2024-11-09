ccab784a09587cf99b88318abfc23057d80f1dff
Liam <liam@fascinated.cc>
2024-11-09 07:04:33 +0000

    re-add logging

---
+++ projects/website/src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse, type NextRequest } from "next/server";
+import { isProduction } from "@ssr/common/utils/utils";
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+
+  // Log requests in production
+  if (isProduction()) {
+    console.log(` ${request.method} ${request.nextUrl.pathname} ${response.status}`);
+  }
+
+  return response;
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
