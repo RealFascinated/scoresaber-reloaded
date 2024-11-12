5839a15440ebb9bf58290c4700ba82875f7b5ae5
Liam <liam@fascinated.cc>
2024-11-12 09:26:09 +0000

    preload stuff

--- projects/website/src/components/preload-resources.tsx
+++ projects/website/src/components/preload-resources.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 
 export function PreloadResources() {
   ReactDOM.prefetchDNS("https://proxy.fascinated.cc");
-  ReactDOM.prefetchDNS("https://scoresber.com");
+  ReactDOM.prefetchDNS("https://analytics.fascinated.cc");
+  ReactDOM.prefetchDNS("https://cdn.scoresaber.com");
   return undefined;
 }
