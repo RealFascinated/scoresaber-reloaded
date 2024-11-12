28451cc800f142120ffa663bbe5d4832fa6afaa1
Liam <liam@fascinated.cc>
2024-11-12 11:55:49 +0000

    cry more

--- projects/backend/src/common/cache.util.ts
+++ projects/backend/src/common/cache.util.ts
@@ -1,6 +1,5 @@
 import { SSRCache } from "@ssr/common/cache";
 import { InternalServerError } from "@ssr/common/error/internal-server-error";
-import { isProduction } from "@ssr/common/utils/utils";
 
 /**
  * Fetches data with caching.
