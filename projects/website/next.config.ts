3d9ef387ea93cb72b8acbdc1f29ffcd78ec5501e
Liam <liam@fascinated.cc>
2024-11-12 15:12:48 +0000

    now????????????

--- projects/website/next.config.ts
+++ projects/website/next.config.ts
@@ -10,11 +10,10 @@ const nextConfig: NextConfig = {
     optimizePackageImports: ["@ssr/common", "@radix-ui/react-icons", "chart.js", "react-chartjs-2"],
     webpackMemoryOptimizations: true,
     reactCompiler: true,
+    cacheHandlers: {
+      default: isProduction() ? require.resolve("/app/projects/website/cache-handler.mjs") : undefined,
+    },
   },
-  cacheHandler: isProduction()
-    ? require.resolve("/app/projects/website/cache-handler.mjs")
-    : require.resolve("./cache-handler.mjs"),
-  cacheMaxMemorySize: 0, // disable default in-memory caching
   images: {
     remotePatterns: [
       {
