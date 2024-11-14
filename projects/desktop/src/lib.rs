eab6537e7fc24f579860c4c9cd40dd6443cbbf1a
Liam <liam@fascinated.cc>
2024-11-14 20:57:38 +0000

    testing the test zone

---
+++ projects/desktop/src/lib.rs
@@ -0,0 +1,12 @@
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(desktop)]
+            app.handle().plugin(tauri_plugin_window_state::Builder::default().build());
+            Ok(())
+        })
+        .plugin(tauri_plugin_shell::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
