8a0cf77447ef51db7c5f625d6a6e47175830f3be
Liam <liam@fascinated.cc>
2024-11-15 11:26:20 +0000

    add desktop app redirect to player page

--- projects/website/src/middleware.ts
+++ projects/website/src/middleware.ts
@@ -3,10 +3,21 @@ import { isProduction } from "@ssr/common/utils/utils";
 
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
+  const playerId = request.cookies.get("playerId");
+  const isDesktopClient = request.headers.get("User-Agent")?.includes("ScoreSaber Reloaded");
+
+  // Handle desktop client redirect
+  if (isDesktopClient) {
+    if (request.nextUrl.pathname === "/" && playerId !== undefined) {
+      return NextResponse.redirect(new URL(`/player/${playerId.value}`, request.url));
+    }
+  }
 
   // Log requests in production
   if (isProduction()) {
-    console.log(` ${request.method} ${request.nextUrl.pathname}${request.nextUrl.search} ${response.status}`);
+    console.log(
+      ` ${isDesktopClient ? "[Desktop App] " : ""}${request.method} ${request.nextUrl.pathname}${request.nextUrl.search} ${response.status}`
+    );
   }
 
   return response;
