407fb382b8b7d5ed205f810baa7043b13219c765
Liam <liam@fascinated.cc>
2024-11-18 17:59:42 +0000

    oops

--- projects/backend/src/controller/image.controller.ts
+++ projects/backend/src/controller/image.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from "elysia-decorators";
 import { t } from "elysia";
 import { ImageService } from "../service/image.service";
+import SSRImage from "../common/ssr-image";
 
 @Controller("/image")
 export default class ImageController {
@@ -22,5 +23,38 @@ export default class ImageController {
   })
   public async getLeaderboardImage({ params: { id } }: { params: { id: string } }) {
     return await ImageService.generateLeaderboardImage(id);
+  }
+
+  @Get("/test", {
+    config: {},
+  })
+  public async test() {
+    const image = new SSRImage({
+      width: 256,
+      height: 256,
+    });
+    await image.setBackgroundImage("https://cdn.fascinated.cc/cFkchQkc.png");
+    image.drawText(
+      [
+        {
+          text: "SSR",
+          color: "#000",
+          fontSize: 42,
+        },
+        {
+          text: "Ranked Maps",
+          color: "#222222",
+          fontSize: 30,
+        },
+      ],
+      "center",
+      0.8
+    );
+
+    return new Response(await image.build(), {
+      headers: {
+        "Content-Type": "image/png",
+      },
+    });
   }
 }
