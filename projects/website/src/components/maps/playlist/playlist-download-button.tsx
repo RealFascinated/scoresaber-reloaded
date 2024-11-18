26e607c5466dac207f64d5473fa8f5fd97ec635a
Liam <liam@fascinated.cc>
2024-11-18 14:42:58 +0000

    bump depends

--- projects/website/src/components/maps/playlist/playlist-download-button.tsx
+++ projects/website/src/components/maps/playlist/playlist-download-button.tsx
@@ -4,7 +4,14 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 type PlaylistDownloadButtonProps = {
+  /**
+   * The name of the playlist
+   */
   name: string;
+
+  /**
+   * The url to download the playlist
+   */
   url: string;
 };
 
