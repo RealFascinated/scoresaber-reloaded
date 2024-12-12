687671e5abd30035640d10414ec89b94974564ba
Lee <liam@fascinated.cc>
2024-12-12 20:30:38 +0000

    fix snow?

--- projects/website/src/components/effects/snow-background.tsx
+++ projects/website/src/components/effects/snow-background.tsx
@@ -3,7 +3,7 @@
 import {Snowfall} from "react-snowfall";
 
 export function SnowBackground() {
-  return <div className="absolute -z-40 w-full h-full pointer-events-none select-none">
+  return <div className="fixed -z-40 w-full h-full pointer-events-none select-none">
     <Snowfall />
   </div>
 }
