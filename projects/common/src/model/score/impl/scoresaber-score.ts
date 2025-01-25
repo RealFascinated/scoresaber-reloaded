b3e99387612f7b5381daca4f2401a23626081fb9
Lee <liam@fascinated.cc>
2025-01-25 23:40:46 +0000

    maybe?

--- projects/common/src/model/score/impl/scoresaber-score.ts
+++ projects/common/src/model/score/impl/scoresaber-score.ts
@@ -115,6 +115,6 @@ export type ScoreSaberPreviousScore = PreviousScore & {
 };
 
 export type ScoreSaberScore = InstanceType<typeof ScoreSaberScorePublic>;
-export type ScoreSaberScoreDocument = ScoreSaberScorePublic & Document;
+export type ScoreSaberScoreDocument = ScoreSaberScore & Document;
 export const ScoreSaberScoreModel: ReturnModelType<typeof ScoreSaberScoreInternal> =
   getModelForClass(ScoreSaberScoreInternal);
