6e902bfbb3b6985b1d332e320c3ab9d5b34d4c3e
Lee <liam@fascinated.cc>
2025-02-01 07:58:07 +0000

    revert

--- projects/website/src/components/player/player-data.tsx
+++ projects/website/src/components/player/player-data.tsx
@@ -14,56 +14,50 @@ import useSettings from "@/hooks/use-settings";
 import dynamic from "next/dynamic";
 import useWindowDimensions from "@/hooks/use-window-dimensions";
 import { ssrApi } from "@ssr/common/utils/ssr-api";
-import { LoadingIcon } from "@/components/loading-icon";
-import { Suspense } from "react";
 
 const Mini = dynamic(() => import("../ranking/mini"), { ssr: false });
 
 type Props = {
-  playerId: string;
+  initialPlayerData: ScoreSaberPlayer;
   initialSearch?: string;
   sort: ScoreSort;
   page: number;
 };
 
-export default function PlayerData({ playerId, initialSearch, sort, page }: Props) {
+export default function PlayerData({ initialPlayerData, initialSearch, sort, page }: Props) {
   const dimensions = useWindowDimensions();
   const database = useDatabase();
   const settings = useSettings();
-  const isFriend = useLiveQuery(() => database.isFriend(playerId));
+  const isFriend = useLiveQuery(() => database.isFriend(initialPlayerData.id));
 
-  const { data: player, isLoading } = useQuery({
-    queryKey: ["playerData", playerId, settings?.playerId, isFriend],
+  let player = initialPlayerData;
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["playerData", player.id, settings?.playerId, isFriend],
     queryFn: async (): Promise<ScoreSaberPlayer | undefined> =>
-      ssrApi.getScoreSaberPlayer(playerId, {
-        createIfMissing: settings?.playerId == playerId,
+      ssrApi.getScoreSaberPlayer(player.id, {
+        createIfMissing: settings?.playerId == player.id,
       }),
   });
 
+  if (data && (!isLoading || !isError)) {
+    player = data;
+  }
+
   return (
-    <div className="flex gap-2 justify-center w-full h-full">
-      {!player && isLoading && (
-        <div className="flex h-full w-full justify-center items-center">
-          <LoadingIcon />
-        </div>
-      )}
-      {player && (
-        <Suspense>
-          <article className="w-full 2xl:w-[1164px] flex flex-col gap-2">
-            <PlayerHeader player={player} />
-            <Card className="gap-1">
-              <PlayerBadges player={player} />
-              {!player.inactive && <PlayerViews player={player} />}
-            </Card>
-            <PlayerScores initialSearch={initialSearch} player={player} sort={sort} page={page} />
-          </article>
-          {dimensions.width > 1536 && !player.inactive && !player.banned && (
-            <aside className="w-[400px] hidden 2xl:flex flex-col gap-2">
-              <Mini type="Global" player={player} />
-              <Mini type="Country" player={player} />
-            </aside>
-          )}
-        </Suspense>
+    <div className="flex gap-2 justify-center w-full">
+      <article className="w-full 2xl:w-[1164px] flex flex-col gap-2">
+        <PlayerHeader player={player} />
+        <Card className="gap-1">
+          <PlayerBadges player={player} />
+          {!player.inactive && <PlayerViews player={player} />}
+        </Card>
+        <PlayerScores initialSearch={initialSearch} player={player} sort={sort} page={page} />
+      </article>
+      {dimensions.width > 1536 && !player.inactive && !player.banned && (
+        <aside className="w-[400px] hidden 2xl:flex flex-col gap-2">
+          <Mini type="Global" player={player} />
+          <Mini type="Country" player={player} />
+        </aside>
       )}
     </div>
   );
