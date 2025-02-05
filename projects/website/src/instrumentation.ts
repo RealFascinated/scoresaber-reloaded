00bfcb7c0b8f92f479f9d4f54741d8d8fae363bc
Lee <liam@fascinated.cc>
2025-02-05 16:26:19 +0000

    fix ?

--- projects/website/src/instrumentation.ts
+++ projects/website/src/instrumentation.ts
@@ -1,27 +1,5 @@
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
-import { Resource } from "@opentelemetry/resources";
-import { NodeSDK } from "@opentelemetry/sdk-node";
-import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-node";
-import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
-import { isProduction } from "@ssr/common/utils/utils";
-
-export function register() {
-  // Only run on server side
-  if (typeof window !== "undefined") return;
-
-  const sdk = new NodeSDK({
-    resource: new Resource({
-      [ATTR_SERVICE_NAME]: "ssr-website",
-      ["deployment.environment"]: isProduction() ? "production" : "development",
-    }),
-    spanProcessors: [
-      new BatchSpanProcessor(
-        new OTLPTraceExporter({
-          url: "https://signoz-injest.fascinated.cc/v1/traces",
-          headers: {}, // Add any required headers here
-        })
-      ),
-    ],
-  });
-  sdk.start();
+export async function register() {
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    await import("./instrumentation.node");
+  }
 }
