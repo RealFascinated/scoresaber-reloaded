2373456221ba7c49c00f06e6607518b5c4e7c563
Lee <liam@fascinated.cc>
2025-02-08 13:21:25 +0000

    more fixes

--- projects/website/src/app/sitemap.ts
+++ projects/website/src/app/sitemap.ts
@@ -1,42 +1,28 @@
-import type { MetadataRoute } from "next";
 import { Config } from "@ssr/common/config";
 import { scoresaberService } from "@ssr/common/service/impl/scoresaber";
-import { SSRCache } from "@ssr/common/cache";
-import { fetchWithCache } from "backend/src/common/cache.util";
-
-const cache = new SSRCache({
-  ttl: 1000 * 60 * 60 * 24 * 3, // 3 days
-});
+import type { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const links: MetadataRoute.Sitemap[] = [];
 
-  links.push(
-    await fetchWithCache(cache, "player-sitemap", async () => {
-      const playerLinks: MetadataRoute.Sitemap[] = [];
+  for (let page = 1; page <= 25; page++) {
+    const playersResponse = await scoresaberService.lookupPlayers(page);
 
-      for (let page = 1; page <= 25; page++) {
-        const playersResponse = await scoresaberService.lookupPlayers(page);
+    if (playersResponse !== undefined) {
+      const players = playersResponse.players;
 
-        if (playersResponse !== undefined) {
-          const players = playersResponse.players;
-
-          playerLinks.push(
-            players.map(player => {
-              return {
-                url: `${Config.websiteUrl}/player/${player.id}`,
-                images: [player.profilePicture],
-                changeFrequency: "daily",
-                priority: 0.5,
-              };
-            })
-          );
-        }
-      }
-
-      return playerLinks.flat();
-    })
-  );
+      links.push(
+        players.map(player => {
+          return {
+            url: `${Config.websiteUrl}/player/${player.id}`,
+            images: [player.profilePicture],
+            changeFrequency: "daily",
+            priority: 0.5,
+          };
+        })
+      );
+    }
+  }
 
   return links.flat();
 }
