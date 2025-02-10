27fe43de3aa8dea436e19e1702c4db090bcec64f
Lee <liam@fascinated.cc>
2025-02-10 16:08:09 +0000

    oops

--- projects/website/src/components/banner/discord-banner.tsx
+++ projects/website/src/components/banner/discord-banner.tsx
@@ -3,15 +3,13 @@ import { FaDiscord } from "react-icons/fa";
 
 export default function DiscordBanner() {
   return (
-    <div className="bg-discord-blue rounded-md text-white px-4 py-1 flex items-center gap-2 justify-center">
+    <Link
+      href="https://discord.gg/kmNfWGA4A8"
+      target="_blank"
+      className="hover:opacity-80 transform-gpu transition-all bg-discord-blue rounded-md text-white px-4 py-1 flex items-center gap-2 justify-center"
+    >
       <FaDiscord />
-      <Link
-        href="https://discord.gg/kmNfWGA4A8"
-        target="_blank"
-        className="hover:opacity-80 transform-gpu transition-all"
-      >
-        <span>Join our Discord server</span>
-      </Link>
-    </div>
+      <span>Join our Discord server</span>
+    </Link>
   );
 }
