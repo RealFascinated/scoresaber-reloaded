1a62ee4a5c76048d2915f37d3462af5dd717d4e2
fascinated <liam@fascinated.cc>
2025-06-15 17:57:19 +0100

    update title

--- projects/website/src/app/(pages)/(ssr)/(pages)/ranking/[[...slug]]/page.tsx
+++ projects/website/src/app/(pages)/(ssr)/(pages)/ranking/[[...slug]]/page.tsx
@@ -37,7 +37,7 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
   const { country } = await getRankingData(props);
 
   const fullCountry = country === undefined ? "Global" : `${getFullCountryName(country)}`;
-  const title = `${fullCountry} / Ranking`;
+  const title = `Ranking / ${fullCountry}`;
   return {
     title: title,
     openGraph: {
