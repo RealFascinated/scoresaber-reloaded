a4c11241dec2ed4e5931a6caebd4138cac010c4e
fascinated <liam@fascinated.cc>
2025-06-19 21:50:02 +0100

    update the skeleton

--- projects/website/src/components/player/mini-ranking/player-mini-ranking-skeleton.tsx
+++ projects/website/src/components/player/mini-ranking/player-mini-ranking-skeleton.tsx
@@ -5,27 +5,32 @@ export function PlayerMiniRankingSkeleton() {
   const skeletonArray = new Array(5).fill(0);
 
   return (
-    <Card className="sticky flex w-[400px] gap-2 select-none">
-      <div className="flex gap-2">
-        <Skeleton className="h-6 w-6 animate-pulse rounded-full" /> {/* Icon Skeleton */}
-        <Skeleton className="h-6 w-32 animate-pulse" /> {/* Text Skeleton for Ranking */}
+    <Card className="sticky flex w-full flex-col gap-2 text-xs select-none sm:w-[400px] sm:text-sm">
+      {/* Header */}
+      <div className="flex items-center gap-2">
+        <Skeleton className="h-5 w-5 animate-pulse" /> {/* Icon Skeleton */}
+        <Skeleton className="h-4 w-20 animate-pulse" /> {/* Text Skeleton for Ranking Type */}
       </div>
 
-      <div className="flex flex-col text-sm">
+      {/* Players List */}
+      <div className="divide-border divide-y">
         {skeletonArray.map((_, index) => (
           <div
             key={index}
-            className="bg-accent grid cursor-pointer grid-cols-[auto_1fr_auto] items-center gap-2 px-2 py-1.5 transition-all first:rounded-t last:rounded-b"
+            className="grid items-center gap-2 px-1 py-1.5 sm:px-2"
+            style={{ gridTemplateColumns: "auto 48px 1fr auto" }}
           >
-            <Skeleton className="h-6 w-12" /> {/* Rank Skeleton */}
-            <div className="flex items-center gap-2">
-              <Skeleton className="h-6 w-6 animate-pulse rounded-full" /> {/* Avatar Skeleton */}
-              <Skeleton className="h-6 w-24 animate-pulse" /> {/* Player Name Skeleton */}
-            </div>
-            <div className="inline-flex min-w-[10.75em] items-center gap-2">
-              <Skeleton className="h-6 w-16 animate-pulse" /> {/* PP Value Skeleton */}
-              <Skeleton className="h-4 w-8 animate-pulse" /> {/* PP Difference Skeleton */}
-            </div>
+            {/* Rank */}
+            <Skeleton className="h-4 w-8 animate-pulse" />
+
+            {/* Avatar */}
+            <Skeleton className="h-6 w-6 animate-pulse rounded-full" />
+
+            {/* Name */}
+            <Skeleton className="h-4 w-24 animate-pulse" />
+
+            {/* PP */}
+            <Skeleton className="h-4 w-16 animate-pulse" />
           </div>
         ))}
       </div>
