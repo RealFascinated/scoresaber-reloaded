c23518eb136563be4aa2ed5bd2becebd7efd6e60
fascinated <liam@fascinated.cc>
2025-06-20 09:04:48 +0100

    make the hmd start use the midnight aigned date

--- projects/backend/src/metrics/impl/player/daily-hmd-statistic.ts
+++ projects/backend/src/metrics/impl/player/daily-hmd-statistic.ts
@@ -1,6 +1,6 @@
 import { Point } from "@influxdata/influxdb-client";
 import { ScoreSaberScoreModel } from "@ssr/common/model/score/impl/scoresaber-score";
-import { TimeUnit } from "@ssr/common/utils/time-utils";
+import { getMidnightAlignedDate, TimeUnit } from "@ssr/common/utils/time-utils";
 import { MetricType } from "../../../service/metrics.service";
 import NumberMetric from "../../number-metric";
 
@@ -13,9 +13,8 @@ export default class HmdStatisticMetric extends NumberMetric {
   }
 
   public async collect(): Promise<Point | undefined> {
-    // Get all scores from the last 24 hours
     const scores = await ScoreSaberScoreModel.find({
-      timestamp: { $gt: new Date(Date.now() - 24 * 60 * 60 * 1000) },
+      timestamp: { $gte: getMidnightAlignedDate(new Date()) }, // Today
     }).select("hmd");
     const hmds = new Map<string, number>();
     for (const score of scores) {
