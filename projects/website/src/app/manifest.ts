7aa2050bf51048e5dadd96dcb8bf39d395c4a12c
fascinated <liam@fascinated.cc>
2025-06-24 15:36:05 +0100

    fix?

--- projects/website/src/app/manifest.ts
+++ projects/website/src/app/manifest.ts
@@ -10,6 +10,21 @@ export default function manifest(): MetadataRoute.Manifest {
     display: "standalone",
     background_color: "#000000",
     theme_color: "#000000",
+    categories: ["scoresaber", "beatsaber"],
+    screenshots: [
+      {
+        src: "https://cdn.fascinated.cc/xmvzAW.png",
+        sizes: "2500x1270",
+        type: "image/png",
+        form_factor: "wide",
+      },
+      {
+        src: "https://cdn.fascinated.cc/NdniRT.png",
+        sizes: "446x994",
+        type: "image/png",
+        form_factor: "narrow",
+      },
+    ],
     icons: [
       {
         src: "/icon-192x192.png",
