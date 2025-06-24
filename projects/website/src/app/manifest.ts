f4b117bebc106f641e9e1cf7dd6a222cadb01821
fascinated <liam@fascinated.cc>
2025-06-24 15:18:34 +0100

    pwaaaa

---
+++ projects/website/src/app/manifest.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "ScoreSaber Reloaded",
+    short_name: "SSR",
+    description:
+      "The ultimate platform for Beat Saber players. Track your progress, compete with friends, and discover detailed insights about your gameplay like never before.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#000000",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
