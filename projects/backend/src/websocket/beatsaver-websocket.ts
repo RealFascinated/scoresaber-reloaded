610fc7319be847f1c43d9361276ff63106d0ce95
fascinated <liam@fascinated.cc>
2025-06-27 14:01:09 +0100

    invalidate cache

--- projects/backend/src/websocket/beatsaver-websocket.ts
+++ projects/backend/src/websocket/beatsaver-websocket.ts
@@ -1,5 +1,6 @@
 import { connectBeatSaverWebsocket } from "@ssr/common/websocket/beatsaver-websocket";
 import BeatSaverService from "../service/beatsaver.service";
+import CacheService from "../service/cache.service";
 
 export class BeatSaverWebsocket {
   constructor() {
@@ -9,6 +10,7 @@ export class BeatSaverWebsocket {
           (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
         )[0].hash;
         await BeatSaverService.createOrUpdateMap(latestHash, map);
+        await CacheService.invalidate(`beatsaver:${latestHash}`); // Invalidate the cache for the map
       },
     });
   }
