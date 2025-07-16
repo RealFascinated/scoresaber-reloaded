f6d083f44e4eac0bbaa6cae13737911629ab5314
fascinated <liam@fascinated.cc>
2025-07-16 11:15:46 +0100

    kitttyyyyyyyy

--- projects/website/src/components/not-found.tsx
+++ projects/website/src/components/not-found.tsx
@@ -12,13 +12,7 @@ type NotFoundProps = {
 export default function NotFound({ title, description }: NotFoundProps) {
   return (
     <div className="flex flex-col items-center gap-6">
-      <Image
-        src="https://cdn.fascinated.cc/assets/get-real-beat-saber.gif"
-        alt="404"
-        className="h-[250px]"
-        width={400}
-        height={250}
-      />
+      <Image src="https://cdn.fascinated.cc/wqmfeQ.gif" alt="404" width={128} height={128} />
 
       <div className="flex flex-col items-center gap-2 text-center">
         <h1 className="text-3xl font-bold text-red-400">{title ?? "Not Found"}</h1>
