b19c762077e295f17cdd638cea5f2e73eef39c38
Lee <liam@fascinated.cc>
2025-07-23 15:40:59 +0100

    add index

--- projects/common/src/model/score/impl/scoresaber-score.ts
+++ projects/common/src/model/score/impl/scoresaber-score.ts
@@ -29,6 +29,7 @@ import { ScoreSaberScoreBase } from "./scoresaber-score-base";
 @index({ leaderboardId: 1 })
 @index({ pp: -1 })
 @index({ timestamp: -1 })
+@index({ playerId: 1, pp: -1 })
 export class ScoreSaberScoreInternal extends ScoreSaberScoreBase {}
 
 export class ScoreSaberScorePublic extends ScoreSaberScoreInternal {
