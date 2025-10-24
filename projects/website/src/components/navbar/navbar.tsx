8a26200b71c79e30710da943979f40dcf3955a0b
Braydon <braydonrainnny@gmail.com>
2025-10-24 08:00:44 -0400

    make this look better bc lee is lazy <3

--- projects/website/src/components/navbar/navbar.tsx
+++ projects/website/src/components/navbar/navbar.tsx
@@ -77,7 +77,7 @@ export default function Navbar() {
   return (
     <nav
       className={cn(
-        "border-border bg-background sticky inset-x-0 top-0 z-50 flex h-12 w-full items-center justify-between border-b px-2 py-1 backdrop-blur-md select-none lg:justify-around lg:px-8"
+        "border-border bg-background/75 backdrop-blur-sm sticky inset-x-0 top-0 z-50 flex h-12 w-full items-center justify-between border-b px-2 py-1 backdrop-blur-md select-none lg:justify-around lg:px-8"
       )}
     >
       {/* Left */}
