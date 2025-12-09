ed4df763efd85efe5b4763da73f4bdd6a95f747e
Lee <liam@fascinated.cc>
2025-12-09 23:24:33 +0000

    add dropshadow to the card

--- projects/website/src/components/card.tsx
+++ projects/website/src/components/card.tsx
@@ -9,7 +9,7 @@ export default function Card({ children, className }: Props) {
   return (
     <div
       className={clsx(
-        "bg-secondary/90 border-muted/50 flex flex-col rounded-(--radius-md) border p-(--spacing-lg)",
+        "bg-secondary/90 border-muted/50 flex flex-col rounded-md border p-(--spacing-lg) drop-shadow-sm",
         className
       )}
     >
