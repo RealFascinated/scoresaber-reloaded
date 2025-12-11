00f654fc29a863c4c2cfb47849bf36c07ba28caf
Lee <liam@fascinated.cc>
2025-12-11 04:05:22 +0000

    try this..?

--- projects/website/src/components/simple-link.tsx
+++ projects/website/src/components/simple-link.tsx
@@ -6,18 +6,28 @@ import { useState } from "react";
 export default function SimpleLink({
   children,
   href,
+  onClick,
+  onMouseEnter,
+  onMouseLeave,
   ...props
-}: { children: React.ReactNode; href: string } & LinkProps & React.ComponentProps<"a">) {
+}: { children: React.ReactNode; href: string } & LinkProps &
+  Omit<React.ComponentProps<"a">, "href">) {
   const [isHovering, setIsHovering] = useState(false);
 
   return (
     <Link
       href={href}
       {...props}
-      // Only prefetch when hovering
-      onMouseEnter={() => setIsHovering(true)}
-      onMouseLeave={() => setIsHovering(false)}
-      prefetch={isHovering}
+      onClick={onClick}
+      onMouseEnter={e => {
+        setIsHovering(true);
+        onMouseEnter?.(e);
+      }}
+      onMouseLeave={e => {
+        setIsHovering(false);
+        onMouseLeave?.(e);
+      }}
+      prefetch={isHovering ? true : undefined}
     >
       {children}
     </Link>
