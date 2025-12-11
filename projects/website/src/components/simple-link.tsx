945b1b5a35d617c107037defd23e241a3d982934
Lee <liam@fascinated.cc>
2025-12-11 04:13:40 +0000

    test without conditional oreloading

--- projects/website/src/components/simple-link.tsx
+++ projects/website/src/components/simple-link.tsx
@@ -1,33 +1,22 @@
 "use client";
 
 import Link, { LinkProps } from "next/link";
-import { useState } from "react";
 
 export default function SimpleLink({
   children,
   href,
-  onClick,
-  onMouseEnter,
-  onMouseLeave,
   ...props
-}: { children: React.ReactNode; href: string } & LinkProps &
-  Omit<React.ComponentProps<"a">, "href">) {
-  const [isHovering, setIsHovering] = useState(false);
+}: { children: React.ReactNode; href: string } & LinkProps & React.ComponentProps<"a">) {
+  // const [isHovering, setIsHovering] = useState(false);
 
   return (
     <Link
       href={href}
       {...props}
-      onClick={onClick}
-      onMouseEnter={e => {
-        setIsHovering(true);
-        onMouseEnter?.(e);
-      }}
-      onMouseLeave={e => {
-        setIsHovering(false);
-        onMouseLeave?.(e);
-      }}
-      prefetch={isHovering ? true : undefined}
+      // Only prefetch when hovering
+      // onMouseEnter={() => setIsHovering(true)}
+      // onMouseLeave={() => setIsHovering(false)}
+      // prefetch={isHovering}
     >
       {children}
     </Link>
