b29f501bda78691e128b5e0b67e97ef2a3078762
Lee <liam@fascinated.cc>
2025-12-14 18:21:20 +0000

    increase match timeout

--- projects/backend/src/websocket/score-websockets.ts
+++ projects/backend/src/websocket/score-websockets.ts
@@ -25,7 +25,7 @@ interface PendingScore {
 }
 
 export class ScoreWebsockets implements EventListener {
-  private static readonly SCORE_MATCH_TIMEOUT = TimeUnit.toMillis(TimeUnit.Minute, 1);
+  private static readonly SCORE_MATCH_TIMEOUT = TimeUnit.toMillis(TimeUnit.Minute, 5);
   private static readonly pendingScores = new Map<string, PendingScore>();
 
   onStop?: () => Promise<void> = async () => {
