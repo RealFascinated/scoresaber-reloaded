6b28a835aedf0e97c8a36d3bb0ac7c113ba7c281
Lee <liam@fascinated.cc>
2025-12-20 16:27:39 +0000

    update prefetch

--- projects/website/src/components/preload-resources.tsx
+++ projects/website/src/components/preload-resources.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { env } from "@ssr/common/env";
+import { CLIENT_PROXY } from "@ssr/common/shared-consts";
 import ReactDOM from "react-dom";
 
 export function PreloadResources() {
-  ReactDOM.prefetchDNS("https://proxy.fascinated.cc");
-  ReactDOM.prefetchDNS("https://analytics.fascinated.cc");
-  ReactDOM.prefetchDNS("https://cdn.scoresaber.com");
+  ReactDOM.prefetchDNS(CLIENT_PROXY);
+  ReactDOM.prefetchDNS(env.NEXT_PUBLIC_API_URL);
+  ReactDOM.prefetchDNS(env.NEXT_PUBLIC_CDN_URL);
   return undefined;
 }
