139c5b00bc2a36640e536a9c90a39694e5b1222e
fascinated <liam@fascinated.cc>
2026-04-01 00:51:46 +0100

    fix type

--- projects/common/src/schemas/response/score/top-scores.ts
+++ projects/common/src/schemas/response/score/top-scores.ts
@@ -1,11 +1,12 @@
 import { PlayerScore } from "../../../score/player-score";
 import { Timeframe } from "../../../timeframe";
+import { ScoreSaberScore } from "../../scoresaber/score/score";
 
 export type TopScoresResponse = {
   /**
    * The top scores.
    */
-  scores: PlayerScore[];
+  scores: PlayerScore<ScoreSaberScore>[];
 
   /**
    * The timeframe returned.
