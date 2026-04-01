fbd2ad68c3e2375687b1b987e53141099d55be89
fascinated <liam@fascinated.cc>
2026-04-01 11:34:03 +0100

    cleanup cache keys file

--- projects/backend/src/common/cache-keys.ts
+++ projects/backend/src/common/cache-keys.ts
@@ -2,9 +2,14 @@ import { DetailType } from "@ssr/common/detail-type";
 import { MapCharacteristic } from "@ssr/common/schemas/map/map-characteristic";
 import { MapDifficulty } from "@ssr/common/schemas/map/map-difficulty";
 
-export function normalizeSongHash(songHash: string): string {
-  return songHash.trim().toLowerCase();
+export function normalizeSongHash(hash: string): string {
+  return hash.trim().toLowerCase();
 }
+
+// Leaderboard
+export const rankedLeaderboardsCacheKey = "leaderboard:ranked-leaderboards";
+export const qualifiedLeaderboardsCacheKey = "leaderboard:qualified-leaderboards";
+export const rankingQueueLeaderboardsCacheKey = "leaderboard:ranking-queue-maps";
 
 export function leaderboardByIdCacheKey(id: number): string {
   return `leaderboard:id:${id}`;
@@ -18,37 +23,13 @@ export function leaderboardByHashCacheKey(
   return `leaderboard:hash:${normalizeSongHash(hash)}:${difficulty}:${characteristic}`;
 }
 
-export const rankedLeaderboardsCacheKey = "leaderboard:ranked-leaderboards";
-export const qualifiedLeaderboardsCacheKey = "leaderboard:qualified-leaderboards";
-export const rankingQueueLeaderboardsCacheKey = "leaderboard:ranking-queue-maps";
-
+// ScoreSaber
 export function playerCacheKey(id: string, type: DetailType): string {
   return `scoresaber:player:${id}:${type}`;
 }
 
 export function cachedPlayerTokenCacheKey(id: string): string {
   return `scoresaber:cached-player:${id}`;
-}
-
-export function beatSaverMapCacheKey(hash: string): string {
-  return `beatsaver:${normalizeSongHash(hash)}`;
-}
-
-export function beatLeaderScoreByIdCacheKey(scoreId: number): string {
-  return `beatleader-score:${scoreId}`;
-}
-
-export function beatLeaderScoreBySongCacheKey(
-  playerId: string,
-  songHash: string,
-  songDifficulty: string,
-  songScore: number
-): string {
-  return `beatleader-score:${playerId}-${songHash.toUpperCase()}-${songDifficulty}-${songScore}`;
-}
-
-export function scoreHistoryGraphCacheKey(playerId: string, leaderboardId: number): string {
-  return `score-history-graph:${playerId}-${leaderboardId}`;
 }
 
 export function scoreSaberApiResponseCacheKey(cacheHash: string): string {
@@ -62,7 +43,31 @@ export function miniRankingCacheKey(
   country?: string
 ): string {
   if (type === "medals") {
-    return `scoresaber:mini-ranking:medals:${playerId}:${page}`;
+    return `scoresaber:mini-ranking:${playerId}:medals:${page}`;
   }
   return `scoresaber:mini-ranking:${playerId}:${type}${type === "country" ? `:${country ?? ""}` : ""}:${page}`;
+}
+
+// BeatSaver
+export function beatSaverMapCacheKey(hash: string): string {
+  return `beatsaver:${normalizeSongHash(hash)}`;
+}
+
+// BeatLeader
+export function beatLeaderScoreByIdCacheKey(scoreId: number): string {
+  return `beatleader-score:${scoreId}`;
+}
+
+export function beatLeaderScoreBySongCacheKey(
+  playerId: string,
+  songHash: string,
+  songDifficulty: string,
+  songScore: number
+): string {
+  return `beatleader-score:${playerId}-${normalizeSongHash(songHash)}-${songDifficulty}-${songScore}`;
+}
+
+// Score history
+export function scoreHistoryGraphCacheKey(playerId: string, leaderboardId: number): string {
+  return `score-history-graph:${playerId}-${leaderboardId}`;
 }
