e45209783629f4284f62653c75a9793b528b7d4b
Fascinated <liam@fascinated.cc>
2026-04-05 15:51:14 +0100

    fix now?

--- projects/common/src/schemas/scoresaber/leaderboard/player-info.ts
+++ projects/common/src/schemas/scoresaber/leaderboard/player-info.ts
@@ -5,8 +5,11 @@ export const ScoreSaberLeaderboardPlayerInfoSchema = z
     id: z.string(),
     name: z.string().optional(),
     country: z.string().optional(),
-    role: z.string().optional(),
-    avatar: z.string().transform(avatar => avatar ?? "https://cdn.fascinated.cc/assets/unknown.png"),
+    role: z.string().nullish(),
+    avatar: z
+      .string()
+      .nullish()
+      .transform(avatar => avatar ?? "https://cdn.fascinated.cc/assets/unknown.png"),
   });
 
 export type ScoreSaberLeaderboardPlayerInfo = z.infer<typeof ScoreSaberLeaderboardPlayerInfoSchema>;
