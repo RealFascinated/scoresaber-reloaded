eac480abf985e82bdb7737fd60d8eff9b968d5f4
fascinated <liam@fascinated.cc>
2026-04-07 18:45:26 +0100

    no need to log these

--- projects/backend/src/websocket/listeners/platform-score-handlers.ts
+++ projects/backend/src/websocket/listeners/platform-score-handlers.ts
@@ -73,7 +73,7 @@ export class ScoreWebsockets implements EventListener {
             `${player.id}-${leaderboard.songHash}-${leaderboard.difficulty.difficulty}-${leaderboard.difficulty.characteristic}`.toUpperCase();
           const pendingScore = ScoreWebsockets.PENDING_SCORES.get(key);
 
-          scoreSaberWsLog.info(`Received score for player ${player.id} with key ${key}`);
+          //scoreSaberWsLog.info(`Received score for player ${player.id} with key ${key}`);
 
           if (pendingScore?.beatLeaderScore) {
             // Found a matching BeatLeader score, process both
@@ -127,7 +127,7 @@ export class ScoreWebsockets implements EventListener {
             `${player.id}-${leaderboard.song.hash}-${leaderboard.difficulty.difficultyName}-${leaderboard.difficulty.modeName}`.toUpperCase();
           const pendingScore = ScoreWebsockets.PENDING_SCORES.get(key);
 
-          beatLeaderWsLog.info(`Received score for player ${player.id}(${player.platform}) with key ${key}`);
+          //beatLeaderWsLog.info(`Received score for player ${player.id}(${player.platform}) with key ${key}`);
 
           if (pendingScore?.scoreSaberToken && pendingScore.leaderboardToken && pendingScore.player) {
             // Found a matching ScoreSaber score, process both
