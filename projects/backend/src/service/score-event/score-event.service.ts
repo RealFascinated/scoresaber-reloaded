2cf0d95ff18a440e91fc0b041dc1d7ca213e1c0c
fascinated <liam@fascinated.cc>
2026-04-07 13:16:22 +0100

    add log

--- projects/backend/src/service/score-event/score-event.service.ts
+++ projects/backend/src/service/score-event/score-event.service.ts
@@ -1,10 +1,16 @@
+import Logger from "@ssr/common/logger";
 import { ScoreSaberScore } from "@ssr/common/schemas/scoresaber/score/score";
-import { TimeUnit } from "@ssr/common/utils/time-utils";
+import { formatDuration, TimeUnit } from "@ssr/common/utils/time-utils";
 import { sql } from "drizzle-orm";
 import { db } from "../../db";
 import { scoreSaberLeaderboardsTable, scoreSaberScoreEventTable } from "../../db/schema";
 
 export class ScoreEventService {
+  /**
+   * Inserts a score event.
+   *
+   * @param score the score to insert the event for.
+   */
   public static async insertScoreEvent(score: ScoreSaberScore): Promise<void> {
     await db.insert(scoreSaberScoreEventTable).values({
       playerId: score.playerId,
@@ -17,6 +23,7 @@ export class ScoreEventService {
    * Recomputes and updates daily plays for all leaderboards using events from the last 24 hours.
    */
   public static async updateLeaderboardDailyPlays(): Promise<void> {
+    const before = performance.now();
     await db.update(scoreSaberLeaderboardsTable).set({
       dailyPlays: sql<number>`(
         SELECT count(*)
@@ -25,5 +32,6 @@ export class ScoreEventService {
           AND events.${scoreSaberScoreEventTable.timestamp} >= ${new Date(Date.now() - TimeUnit.toMillis(TimeUnit.Day, 1))}
       )`,
     });
+    Logger.info(`Updated leaderboard daily plays in ${formatDuration(performance.now() - before)}`);
   }
 }
