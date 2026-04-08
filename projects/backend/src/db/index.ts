b2307bcf5334bbfdd655fbe2dca385303b29c3d5
fascinated <liam@fascinated.cc>
2026-04-08 08:17:33 +0100

    fix

--- projects/backend/src/db/index.ts
+++ projects/backend/src/db/index.ts
@@ -1,13 +1,12 @@
 import { env } from "@ssr/common/env";
-import "dotenv/config";
 import { SQL } from "bun";
+import "dotenv/config";
 import { drizzle } from "drizzle-orm/bun-sql";
 import * as schema from "./schema";
 
 const client = new SQL({
   url: env.DATABASE_URL,
   max: 50, // max connections in pool
-  idleTimeout: 30, // seconds (close idle connections after 30s)
   connectionTimeout: 2, // seconds (timeout when establishing new connections)
 });
 
